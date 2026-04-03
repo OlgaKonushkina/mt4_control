a7a973df6859e5f81a3d289ed491119d68749f13
OlgaKonushkina <ol_ya_97@mail.ru>
2026-04-03 15:53:42 +0300

    fix: добавили миграцию Android embedding v2

--- android/app/src/main/kotlin/com/example/mt4_control/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mt4_control/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.mt4_control;
+package com.example.mt4_control
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity
 
-public class MainActivity extends FlutterActivity {
-}
+class MainActivity: FlutterActivity()
